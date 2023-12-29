a92965abaf634f1a4cc672a4ed1510980ea305b9
DarioTek <dmontecastro@yahoo.com>
2023-12-28 19:13:09 -0500

    Update SPX_OHLC_MINUTES_Queries.sql

--- SPX_OHLC_MINUTES_Queries.sql
+++ SPX_OHLC_MINUTES_Queries.sql
@@ -1,6 +1,26 @@
+/* Use dariotek schema */
 use dariotek;
 
+# Generic select all records from the table
 select * from dariotek.SPX_OHLC_MINUTES;
+
+-- is another way to comment in MySQL
+select count(*) from dariotek.SPX_OHLC_MINUTES;
+
+/*
+use the date function to get the date from the Datetime column
+*/
+select date(Datetime) from dariotek.SPX_OHLC_MINUTES;
+
+/*
+check the count of minutes record for each day 
+typically 390 in a full trading day
+typically 210 in a shortened trading day
+*/
+select date(Datetime), count(*) from dariotek.SPX_OHLC_MINUTES
+GROUP BY date(Datetime)
+ORDER BY date(Datetime);
+
 
 SELECT * FROM dariotek.SPX_OHLC_MINUTES 
 WHERE Datetime BETWEEN '2023-11-09 09:30:00' AND '2023-11-09 10:30:00';
