ad9fed13d6077def112a10024ef84e975e8b3d41
DarioTek <dmontecastro@yahoo.com>
2024-05-10 17:32:44 -0400

    Update RaspberryPi_mySQL.sql

--- RaspberryPi_mySQL.sql
+++ RaspberryPi_mySQL.sql
@@ -12,15 +12,27 @@ select date(Datetime), count(*) from SPX_OHLC_MINUTES
 GROUP BY date(Datetime)
 ORDER BY date(Datetime);
 
+select Ticker, date(Datetime), count(*) from OHLC_MINUTES
+GROUP BY Ticker, date(Datetime)
+ORDER BY Ticker, date(Datetime);
+
+
 select * from SPX_OHLC_MINUTES where date(Datetime) >= '2024-05-02';
 
 ALTER TABLE SPX_OHLC_MINUTES ADD PRIMARY KEY (Datetime);
 
 select * from OHLC_MINUTES;
 
-select Ticker, date(Datetime), count(*) from OHLC_MINUTES
-GROUP BY Ticker, date(Datetime)
-ORDER BY Ticker, date(Datetime);
+/*
+ * Add Composite Key
+ */
+ALTER TABLE OHLC_MINUTES ADD PRIMARY KEY (Ticker, Datetime);
+
+SHOW KEYS FROM OHLC_MINUTES WHERE Key_name = 'PRIMARY';
+
+SHOW KEYS FROM SPX_OHLC_MINUTES WHERE Key_name = 'PRIMARY';
+
+
 
 select count(*) from OHLC_MINUTES where Ticker = '^SPX';
 
@@ -29,4 +41,4 @@ select count(*) from OHLC_MINUTES where Ticker = '^SPX';
 #delete from SPX_OHLC_MINUTES where date(Datetime) >= '2024-05-02';
 #commit;
 
-#SET SQL_SAFE_UPDATES = 0;
+#SET SQL_SAFE_UPDATES = 1;
