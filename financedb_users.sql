4111d52e8812493b06e31790ba2ebf13bbaadaac
DarioTek <dmontecastro@yahoo.com>
2024-08-05 23:18:06 -0400

    Update financedb_users.sql

--- financedb_users.sql
+++ financedb_users.sql
@@ -4,6 +4,9 @@ SHOW GRANTS FOR 'root'@'%';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'Dabb1234y1!';
+GRANT ALL PRIVILEGES ON financedb.* TO 'roor'@'localhost';
 
 CREATE DATABASE financedb;
 
@@ -12,6 +15,10 @@ CREATE USER 'remote_user'@'localhost' IDENTIFIED BY 'Dabb1234y1!';
 
 GRANT ALL PRIVILEGES ON financedb.* TO 'finance_user'@'localhost';
 GRANT ALL PRIVILEGES ON financedb.* TO 'remote_user'@'localhost';
+
+
+GRANT ALL PRIVILEGES ON financedb.* TO 'root'@'%';
+FLUSH PRIVILEGES;
 
 
 
