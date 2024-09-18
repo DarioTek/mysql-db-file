95b81f718d26155a0399be044cebef7a91d2d6c0
DarioTek <dmontecastro@yahoo.com>
2024-09-17 20:20:42 -0400

    Update LOAD_TRANSACTION_HISTORY.sql

--- LOAD_TRANSACTION_HISTORY.sql
+++ LOAD_TRANSACTION_HISTORY.sql
@@ -1,5 +1,28 @@
 use financedb;
 
+/*
+ * All Transactions for each of the 4 accounts
+ */
+SELECT 
+	*
+FROM
+	LOAD_TRANSACTION_HISTORY;
+    
+
+/*
+ * Number of transactions for 2024 
+ */
+SELECT 
+	SOURCE,
+    COUNT(SOURCE)
+FROM
+	LOAD_TRANSACTION_HISTORY
+WHERE
+	RECORD_DATE >= '2024-01-01'
+GROUP BY 
+	SOURCE;
+
+
 SELECT 
 	*
 FROM
